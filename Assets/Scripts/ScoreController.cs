1c42d277bb19b508fbb4d781d1a58f7ca0dfd6e3
Tanel Peet <saiber@gmail.com>
2015-12-05 18:10:09 +0000

    Added new prefabs and made resources bar

--- Assets/Scripts/ScoreController.cs
+++ Assets/Scripts/ScoreController.cs
@@ -28,30 +28,83 @@ public class ScoreController : MonoBehaviour
         set
         {
             _currentResources = value;
-            HandleHealth();
+            HandleResources();
         }
     }
 
 
     public RectTransform HealthTransform;
-    private float CachedY;
-    private float MinX;
-    private float MaxX;
+    private float HealthCachedY;
+    private float HealthMinX;
+    private float HealthMaxX;
     public Text HealthText;
     public Image VisualHealth;
 
-    //
-    public float CoolDown;
-    private bool OnCoolDown;
+
+
+    public RectTransform ResourcesTransform;
+    private float ResourcesCachedY;
+    private float ResourcesMinX;
+    private float ResourcesMaxX;
+    public Text ResourcesText;
+    public Image VisualResources;
+
+    // Sweating
+    public int SweatHealthBonus = 20;
+    public int SweatTimeout = 30;
+    private Button _sweatButton;
+
+    // Repair variables
+    private Button _repairButton;
+    public int RepairBonus = 20;
+    public int RepairResources = 30;
+
+    // Neutrophil variables
+    private Button _neutrophilButton;
+    public int NeutrophilCost = 30;
+
+    // Helper T variables
+    private Button _healerButton;
+    public int HealerCost = 10;
+
+
 
     void Start ()
     {
-        CachedY = HealthTransform.position.y;
-        MaxX = HealthTransform.position.x;
-        MinX = HealthTransform.position.x - HealthTransform.rect.width;
+        //Healthbar init
+        HealthCachedY = HealthTransform.position.y;
+        HealthMaxX = HealthTransform.position.x;
+        HealthMinX = HealthTransform.position.x - HealthTransform.rect.width;
         CurrentOverallHealth = maxHealth;
-        OnCoolDown = false;
+
+        //ResourceBar init
+        ResourcesCachedY = ResourcesTransform.position.y;
+        ResourcesMaxX = ResourcesTransform.position.x;
+        ResourcesMinX = ResourcesTransform.position.x -ResourcesTransform.rect.width;
+        CurrentResources = maxResources;
+
+        // Buttons init
         _sweatButton = GameObject.Find("SweatButton").GetComponent<Button>();
+        _repairButton = GameObject.Find("RepairButton").GetComponent<Button>();
+        _healerButton = GameObject.Find("HelperTButton").GetComponent<Button>();
+        _neutrophilButton = GameObject.Find("NeutrophilButton").GetComponent<Button>();
+
+        OnCoolDown = false;
+    }
+
+    public void Repair()
+    {
+        CurrentResources -= 5;
+    }
+
+    public void CreateHealer()
+    {
+        
+    }
+
+    public void CreateNeutrophil()
+    {
+        
     }
 	
 	// Update is called once per frame
@@ -63,6 +116,10 @@ public class ScoreController : MonoBehaviour
 	    }
 	}
 
+
+
+    public float CoolDown;
+    private bool OnCoolDown;
     IEnumerator CoolDownDamage()
     {
         OnCoolDown = true;
@@ -70,11 +127,14 @@ public class ScoreController : MonoBehaviour
         OnCoolDown = false;
     }
 
+
+
+    #region HealthBar movement
     private void HandleHealth()
     {
         HealthText.text = "Health: " + CurrentOverallHealth;
-        var currentX = MapHealthBarValues(CurrentOverallHealth, 0, maxHealth, MinX, MaxX);
-        HealthTransform.position = new Vector3(currentX, CachedY);
+        var currentX = MapHealthBarValues(CurrentOverallHealth, 0, maxHealth, HealthMinX, HealthMaxX);
+        HealthTransform.position = new Vector3(currentX, HealthCachedY);
 
         // more than 50% health
         if (CurrentOverallHealth > maxHealth/2)
@@ -96,11 +156,45 @@ public class ScoreController : MonoBehaviour
         return (x - inMin)*(outMax - outMin)/(inMax - inMin) + outMin;
     }
 
+    #endregion
 
-    // Sweating
-    public int SweatHealthBonus = 20;
-    public int SweatTimeout = 30;
-    private Button _sweatButton;
+
+
+
+
+
+    #region ResourceBar movement
+    private void HandleResources()
+    {
+        ResourcesText.text = "Resources: " + CurrentResources;
+        var currentX = MapHealthBarValues(CurrentResources, 0, maxResources, ResourcesMaxX, ResourcesMinX) + ResourcesTransform.rect.width;
+        ResourcesTransform.position = new Vector3(currentX, ResourcesCachedY);
+
+        // more than 50% health
+        if (CurrentResources > maxResources / 2)
+        {
+            var redChannelValue = MapHealthBarValues(CurrentResources, maxResources / 2, maxResources, 255, 0);
+            VisualResources.color = new Color32((byte)redChannelValue, 255, 0, 255);
+        }
+
+        //less than 50% health
+        else
+        {
+            var greenChannelValue = MapHealthBarValues(CurrentResources, 0, maxResources / 2, 0, 255);
+            VisualResources.color = new Color32(255, (byte)greenChannelValue, 0, 255);
+        }
+    }
+
+    #endregion
+
+
+
+
+
+
+
+    #region Sweating
+
 
     public void Sweat()
     {
@@ -118,6 +212,6 @@ public class ScoreController : MonoBehaviour
     }
 
 
-
+    #endregion
 
 }
